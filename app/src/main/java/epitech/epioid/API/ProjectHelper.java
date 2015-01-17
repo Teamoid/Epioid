3abb20e19d4ae2f759d727b82d162cb02c1fbddb
Michel Antoine <antoin_m@epitech.eu>
2015-01-17 16:13:12 +0100

    Can now get /projects (FIX).

--- app/src/main/java/epitech/epioid/API/ProjectHelper.java
+++ app/src/main/java/epitech/epioid/API/ProjectHelper.java
@@ -3,11 +3,9 @@ package epitech.epioid.API;
 import android.util.Log;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import epitech.epioid.API.Items.Projects;
@@ -19,29 +17,24 @@ public class ProjectHelper {
     private static final String TAG = "ProjectHelper";
 
     public static void getProjects(final EpitechApiCallback callback) {
-        RequestParams requestParams = new RequestParams();
-        requestParams.put("token", Epitech.getToken());
-
-        Epitech.client.post(Epitech.URL + "/projects", requestParams, new JsonHttpResponseHandler() {
+        Epitech.client.get(Epitech.URL + "/projects?token=" + Epitech.getToken(), new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response)
-            {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 callback.callBack(null);
             }
 
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response)
-            {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 Projects projects = new Projects();
 
                 for (int i = 0; i < response.length(); i++) {
                     try {
-                        projects.items.add((Projects.Project)Epitech.parseJSON(response.getJSONObject(i), Projects.Project.class));
-                    } catch (JSONException e) {
+                        projects.items.add((Projects.Project) Epitech.parseJSON(response.getJSONObject(i), Projects.Project.class));
+                    } catch (Exception e) {
                         Log.e(TAG, e.toString(), e);
                     }
-                    callback.callBack(projects);
                 }
+                callback.callBack(projects);
             }
 
             @Override
